e86c8d6b1116bb9409dc4aff2c43144fb45ad3d5
Dzmitry Kasareuski <dzmitrykasareuski@gmail.com>
2023-05-20 13:58:51 +0200

    fix: api request

--- src/api/categories/getCategories.ts
+++ src/api/categories/getCategories.ts
@@ -6,13 +6,7 @@ const getCategories = async () => {
 
     return axios<CategoryType[]>({
         method: "GET",
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "*",
-            "Access-Control-Allow-Credentials": "true",
-        },
-        // url: "http://localhost:3001/categories",
-        url: "https://kosarevsky-ideal-guacamole-jgqr6pq567q2p4jp-3000.preview.app.github.dev/categories",
+        url: "http://localhost:3001/categories",
     });
 };
 
--- src/api/products/getProducts.ts
+++ src/api/products/getProducts.ts
@@ -5,8 +5,7 @@ import ParamsApiType from "../../types/paramsApiType";
 const getProducts = async (filter: ParamsApiType) => {
     return axios<ProductType[]>({
         method: "GET",
-        url: "https://kosarevsky-ideal-guacamole-jgqr6pq567q2p4jp-3000.preview.app.github.dev/products",
-        // url: "http://localhost:3001/products",
+        url: "http://localhost:3001/products",
         params: {
             _sort: filter.sortField,
             _order: filter.sortDir,
